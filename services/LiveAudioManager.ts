d8d074d4e321f5f38f47dff391fb0a844322129f
Neel Chaurasiya <neel.c.chaurasiya@gmail.com>
2026-05-29 23:48:00 +0530

    added audio-part

--- services/LiveAudioManager.ts
+++ services/LiveAudioManager.ts
@@ -1,15 +1,21 @@
-import { MODEL } from "@/lib/constants";
+import { INPUT_SAMPLE_RATE, MODEL, } from "@/lib/constants";
 import { GoogleGenAI, Modality, Session } from "@google/genai";
 
 export class LiveAudioManager {
   private ai: GoogleGenAI;
   private ActiveSession: Session | null = null;
+  private InputAudioContext : AudioContext | null = null;
+  private OutputAudioContext : AudioContext | null = null;
+  private OutputNode : GainNode | null = null;
+  private MediaStream : MediaStream | null = null;
+
 
   constructor() {
     this.ai = new GoogleGenAI({
-      apiKey: "AIzaSyDFpJCnqgwTwIhlAyKS5S7eN2ymm1PPrws",
+      apiKey: process.env.NEXT_PUBLIC_API_KEY,
     });
   }
+
   async StartSession() {
     const config = { responseModalities: [Modality.AUDIO] };
     this.ActiveSession = await this.ai.live.connect({
@@ -30,6 +36,32 @@ export class LiveAudioManager {
       },
       config: config,
     });
+
+    this.InputAudioContext = new AudioContext();
+    this.OutputAudioContext = new AudioContext();
+
+    if(this.InputAudioContext.state === "suspended"){
+      this.InputAudioContext.resume();
+    }
+
+    if(this.OutputAudioContext.state === "suspended"){
+      this.OutputAudioContext.resume();
+    }
+
+    this.OutputNode = this.OutputAudioContext.createGain()
+
+    this.OutputNode.connect(this.OutputAudioContext.destination)
+
+    this.MediaStream = await navigator.mediaDevices.getUserMedia({
+      audio : {
+        sampleRate : INPUT_SAMPLE_RATE,
+        channelCount : 1,
+        echoCancellation : true,
+        noiseSuppression : true,
+        autoGainControl : true,
+      }
+    })
+
     console.log(this.ActiveSession);
     
   }
